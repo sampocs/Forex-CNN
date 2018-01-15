bb0c4fe6a51931b58e877ae766655d1f565da305
sampocs <spochyly@wisc.edu>
2018-01-14 21:35:05 -0600

    tuning

--- class_network.py
+++ class_network.py
@@ -21,7 +21,7 @@ num_fc = 128
 flat_nodes = 3200
 #learning_rate = 0.001
 epoch_len = 3000
-num_epochs = 5
+num_epochs = 100
 
 def get_batch(data, batch_size=batch_size):
 	x, y = data
@@ -52,90 +52,91 @@ def conv2d(x, W):
 def max_pool(x, size):
 	return tf.nn.max_pool(x, ksize=[1, size, 1, 1], strides=[1, size, 1, 1], padding='SAME')
 
+#INPUT/LABEL
+#X-dim: Batch, height, width, depth
+x = tf.placeholder(tf.float32, shape=[None, x_dim[0], x_dim[1], 1], name='x')
+#Y-dim: batch, one-hot class (sell, hold, buy) 
+y_ = tf.placeholder(tf.float32, shape=[None, num_classes], name='y')
+
+#WEIGHTS/BIAS
+W_conv1 = conv_weights([4, 40, 1, 16], 'W_conv1')
+W_conv2 = conv_weights([4, 1, 16, 16], 'W_conv2')
+W_conv3 = conv_weights([3, 1, 16, 32], 'W_conv3')
+W_conv4 = conv_weights([3, 1, 32, 32], 'W_conv4')
+
+b_conv1 = bias([16], 'b_conv1')
+b_conv2 = bias([16], 'b_conv2')
+b_conv3 = bias([32], 'b_conv3')
+b_conv4 = bias([32], 'b_conv4')
+
+W_fc1 = fc_weights([flat_nodes, num_fc], 'W_fc1')
+W_fc2 = fc_weights([num_fc, 3], 'W_fc2') 
+b_fc1 = bias([num_fc], 'b_fc1')
+b_fc2 = bias([3], 'b_fc2')
+
+
+#BUILD
+#Layer 1: Convolution -> batch normalization -> relu 
+l1_conv = conv2d(x, W_conv1) + b_conv1
+l1_bn = tf.layers.batch_normalization(l1_conv)
+l1_prime = tf.nn.relu(l1_bn)
+
+#Layer 2: Convolution -> batch normalization -> relu -> max pooling
+l2_conv = conv2d(l1_prime, W_conv2) + b_conv2
+l2_bn = tf.layers.batch_normalization(l2_conv)
+l2_prime = tf.nn.relu(l2_bn)
+l2_pool = max_pool(l2_prime, 2)
+
+#Layer 3: Convolution -> batch normalization -> relu
+l3_conv = conv2d(l2_pool, W_conv3) + b_conv3
+l3_bn = tf.layers.batch_normalization(l3_conv)
+l3_prime = tf.nn.relu(l3_bn)
+
+#Layer 4: Convolution -> batch normalization -> relu -> max pooling
+l4_conv = conv2d(l3_prime, W_conv4) + b_conv4
+l4_bn = tf.layers.batch_normalization(l4_conv)
+l4_prime = tf.nn.relu(l4_bn)
+l4_pool = max_pool(l4_prime, 2)
+
+#Layer 5: Flatten -> fully connected -> relu -> fully connected
+l5_flat = tf.reshape(l4_pool, [-1, flat_nodes])
+l5_fc = tf.matmul(l5_flat, W_fc1) + b_fc1
+l5_prime = tf.nn.relu(l5_fc)
+y = tf.add(tf.matmul(l5_prime, W_fc2), b_fc2, name="y")
+
+#Loss: Softmax -> cross entropy loss -> average -> ADAM Optimizer
+with tf.name_scope("loss"):
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+lr = tf.placeholder(tf.float32)
+optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+train_step = optimizer.minimize(loss)
+
+#Accuracy
+with tf.name_scope("metrics"):
+	correct_predicition = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+	accuracy = tf.reduce_mean(tf.cast(correct_predicition, tf.float32))
+	#precision = tf.metrics.precision(y_, y)
+	#recall = tf.metrics.recall(y_, y)
+
+#Tensorboard 
+#Store histograms
+tf.summary.histogram("W_conv1", W_conv1)
+tf.summary.histogram("W_conv2", W_conv2)
+tf.summary.histogram("W_conv3", W_conv3)
+tf.summary.histogram("W_conv4", W_conv4)
+tf.summary.histogram("fc_1", W_fc1)
+tf.summary.histogram("fc_2", W_fc2)
+
+#Loss/Accuracy/Precision/Recall
+tf.summary.scalar("Loss", loss)
+tf.summary.scalar("Accuracy", accuracy)
+#tf.summary.scalar("Precision", precision)
+#tf.summary.scalar("Recall", recall)
+
 tuning = ''
 for _ in range(100):
-	learning_rate = 10 ** uniform(-6, -2)
-	print "Learning Rate: {}".format(learning_rate)
-	#INPUT/LABEL
-	#X-dim: Batch, height, width, depth
-	x = tf.placeholder(tf.float32, shape=[None, x_dim[0], x_dim[1], 1], name='x')
-	#Y-dim: batch, one-hot class (sell, hold, buy) 
-	y_ = tf.placeholder(tf.float32, shape=[None, num_classes], name='y')
-
-	#WEIGHTS/BIAS
-	W_conv1 = conv_weights([4, 40, 1, 16], 'W_conv1')
-	W_conv2 = conv_weights([4, 1, 16, 16], 'W_conv2')
-	W_conv3 = conv_weights([3, 1, 16, 32], 'W_conv3')
-	W_conv4 = conv_weights([3, 1, 32, 32], 'W_conv4')
-
-	b_conv1 = bias([16], 'b_conv1')
-	b_conv2 = bias([16], 'b_conv2')
-	b_conv3 = bias([32], 'b_conv3')
-	b_conv4 = bias([32], 'b_conv4')
-
-	W_fc1 = fc_weights([flat_nodes, num_fc], 'W_fc1')
-	W_fc2 = fc_weights([num_fc, 3], 'W_fc2') 
-	b_fc1 = bias([num_fc], 'b_fc1')
-	b_fc2 = bias([3], 'b_fc2')
-
-
-	#BUILD
-	#Layer 1: Convolution -> batch normalization -> relu 
-	l1_conv = conv2d(x, W_conv1) + b_conv1
-	l1_bn = tf.layers.batch_normalization(l1_conv)
-	l1_prime = tf.nn.relu(l1_bn)
-
-	#Layer 2: Convolution -> batch normalization -> relu -> max pooling
-	l2_conv = conv2d(l1_prime, W_conv2) + b_conv2
-	l2_bn = tf.layers.batch_normalization(l2_conv)
-	l2_prime = tf.nn.relu(l2_bn)
-	l2_pool = max_pool(l2_prime, 2)
-
-	#Layer 3: Convolution -> batch normalization -> relu
-	l3_conv = conv2d(l2_pool, W_conv3) + b_conv3
-	l3_bn = tf.layers.batch_normalization(l3_conv)
-	l3_prime = tf.nn.relu(l3_bn)
-
-	#Layer 4: Convolution -> batch normalization -> relu -> max pooling
-	l4_conv = conv2d(l3_prime, W_conv4) + b_conv4
-	l4_bn = tf.layers.batch_normalization(l4_conv)
-	l4_prime = tf.nn.relu(l4_bn)
-	l4_pool = max_pool(l4_prime, 2)
-
-	#Layer 5: Flatten -> fully connected -> relu -> fully connected
-	l5_flat = tf.reshape(l4_pool, [-1, flat_nodes])
-	l5_fc = tf.matmul(l5_flat, W_fc1) + b_fc1
-	l5_prime = tf.nn.relu(l5_fc)
-	y = tf.add(tf.matmul(l5_prime, W_fc2), b_fc2, name="y")
-
-	#Loss: Softmax -> cross entropy loss -> average -> ADAM Optimizer
-	with tf.name_scope("loss"):
-		loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-	train_step = optimizer.minimize(loss)
-
-	#Accuracy
-	with tf.name_scope("metrics"):
-		correct_predicition = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-		accuracy = tf.reduce_mean(tf.cast(correct_predicition, tf.float32))
-		#precision = tf.metrics.precision(y_, y)
-		#recall = tf.metrics.recall(y_, y)
-
-	#Tensorboard 
-	#Store histograms
-	tf.summary.histogram("W_conv1", W_conv1)
-	tf.summary.histogram("W_conv2", W_conv2)
-	tf.summary.histogram("W_conv3", W_conv3)
-	tf.summary.histogram("W_conv4", W_conv4)
-	tf.summary.histogram("fc_1", W_fc1)
-	tf.summary.histogram("fc_2", W_fc2)
-
-	#Loss/Accuracy/Precision/Recall
-	tf.summary.scalar("Loss", loss)
-	tf.summary.scalar("Accuracy", accuracy)
-	#tf.summary.scalar("Precision", precision)
-	#tf.summary.scalar("Recall", recall)
-
+	learn_r = 10 ** uniform(-6, -2)
+	print "Learning Rate: {}".format(learn_r)
 	with tf.Session() as sess:
 		train_writer = tf.summary.FileWriter("/tmp/forex_cnn/train", sess.graph)
 		val_writer = tf.summary.FileWriter("/tmp/forex_cnn/val")
@@ -152,25 +153,26 @@ for _ in range(100):
 				if j % 1000 == 0 and j != epoch_len:
 					pass
 					#batch_loss, _ = sess.run([loss, train_step],
-					#							feed_dict={ x: batch_x, y_: batch_y })
+					#							feed_dict={ x: batch_x, y_: batch_y, lr: learn_r })
 					#print "Iteration {}/{} for Epoch {}, Loss: {}".format(j, epoch_len, i, batch_loss)
 				
 				#Report batch accuracy after each epoch
 				elif j == epoch_len:
 					x_train_batch, y_train_batch = get_batch(train, 50000)
-					summary, train_acc, train_loss = sess.run([merged, accuracy, loss], feed_dict= { x: x_train_batch, y_: y_train_batch })
+					summary, train_acc, train_loss = sess.run([merged, accuracy, loss], feed_dict= { x: x_train_batch, y_: y_train_batch, lr: learn_r })
 					train_writer.add_summary(summary, i)
 
-					summary, val_acc = sess.run([merged, accuracy], feed_dict={ x: x_val, y_: y_val })
+					summary, val_acc = sess.run([merged, accuracy], feed_dict={ x: x_val, y_: y_val, lr: learn_r })
 					val_writer.add_summary(summary, i)
 
 					print "Epoch: {}, Loss: {}, Train Acc: {}, Val Acc: {}".format(i, train_loss, train_acc, val_acc)
+
 					with open("tuning.txt", 'w') as w_file:
 						tuning += "Learning Rate: {}, Epoch: {}, Loss: {}, Train Acc: {}, Val Acc: {}\n".format(learning_rate, i, train_loss, train_acc, val_acc)
 						w_file.write(tuning)
 						
 				else:
-					_ = sess.run([train_step], feed_dict={ x: batch_x, y_: batch_y })
+					_ = sess.run([train_step], feed_dict={ x: batch_x, y_: batch_y, lr: learn_r })
 
 #tensorboard --logdir /tmp/forex_cnn/
 
